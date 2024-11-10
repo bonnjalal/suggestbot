e585c7f43deae118e0faf245b69d02177bc74ea4
bonnjalal <bonnjalal@gmail.com>
2024-11-10 20:13:26 +0000

    testing coedits

--- tests/test_coedits.py
+++ tests/test_coedits.py
@@ -4,6 +4,13 @@
 Test the XML-RPC-based co-edit recommender.
 '''
 
+import sys
+import os
+
+# Add the parent directory to the Python path
+# Use this line only if your want to test the script directly from the current path 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import logging
 
 from suggestbot import config
